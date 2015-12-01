aec8e5ae2f1938bfbdcc42c9435e91f3dcfbb63b
uGuardian <heavyse.sea0310@csimac3-3.local>
2015-12-01 14:21:59 -0700

    Here we go!

--- Stuff/main.cpp
+++ Stuff/main.cpp
@@ -5,13 +5,8 @@ using namespace std;
 
 int main()
 {
-    string input;
-    cout << "Input here:";
-    getline (cin, input);
-    
-    size_t up = count(input.begin(), input.end(), '(');
-    size_t down = count(input.begin(), input.end(), ')');
-    int out = up - down;
-    
-    cout << "The answer is " << out;
+    cout << "Please enter your string.";
+    string input[1];
+    getline(cin, input[0]);
+    cout << "Continute?"
 }
