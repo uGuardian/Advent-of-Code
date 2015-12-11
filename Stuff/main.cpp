73a41e0a6b0297a54945999adbf1e3a608700817
uGuardian <Sean.Heavysege@Gmail.com>
2015-12-10 20:35:43 -0700

    Update

--- Stuff/main.cpp
+++ Stuff/main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
+#include <boost/algorithm/string.hpp>
 using namespace std;
+using namespace boost;
 
 int main()
 {
+  char question();
+  vector<string> input;
+  bool response() = true;
+  int number() = 0
+  while (response = true)
+  {
     cout << "Please enter your string.";
-    string input[1];
-    getline(cin, input[0]);
-    cout << "Continute?"
-}
+    getline(cin, input[number]);
+    bool valid() = false;
+    {
+      cout << "Continue input? [Y/N]"
+      getline(cin, question);
+      if (question = y) {
+        response = true;
+        number = number + 1;
+        valid = true;
+      }
+      elseif (question = n) {
+        response = false;
+        valid = true;
+      }
+      else {
+        cout << "Invalid response, please try again."
+      }
+    }
+  }
